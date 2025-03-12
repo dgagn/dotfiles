6f1f9057bcbc198902b3e1075f72835984fa3765
Dany Gagnon <admin@ovior.ca>
2025-03-11 20:41:02 -0400

    remove live grep args

--- editor/nvim/lua/ovior/plugin/telescope.lua
+++ editor/nvim/lua/ovior/plugin/telescope.lua
@@ -3,14 +3,13 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-live-grep-args.nvim",
       "nvim-telescope/telescope-ui-select.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
     },
     config = function(_, opts)
       require("telescope").setup(opts)
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
-      pcall(require("telescope").load_extension, "live_grep_args")
     end,
     keys = function()
       local builtin = require("telescope.builtin")
@@ -21,102 +20,13 @@ return {
           builtin.find_files,
         },
         {
-          "<leader>fF",
-          function()
-            builtin.find_files({
-              previewer = false,
-              find_command = { "fd", "--type", "f", "--hidden", "--follow", "--no-ignore" },
-            })
-          end,
-        },
-        {
-          "<leader>fb",
-          builtin.buffers,
-        },
-        {
           "<leader>fg",
           builtin.live_grep,
         },
         {
-          "<leader>fA",
-          function()
-            builtin.live_grep({
-              no_ignore = false,
-              search_dirs = { vim.fn.expand("%:p:h") },
-            })
-          end,
-        },
-        {
-          "<leader>fw",
-          builtin.grep_string,
-        },
-        {
-          "<leader>fD",
-          builtin.diagnostics,
-        },
-        {
           "<leader>fh",
           builtin.help_tags,
-        },
-        {
-          "<leader>?",
-          builtin.oldfiles,
-        },
-        {
-          "<leader>hh",
-          builtin.help_tags,
-        },
-        {
-          "<leader>fd",
-          function()
-            local cwd = vim.fn.expand("%:p:h")
-            if vim.bo.filetype == "oil" then
-              cwd = string.gsub(cwd, "^oil://", "")
-            end
-            builtin.find_files({
-              cwd = cwd,
-              find_command = { "fd", "--type", "d", "--hidden", "--follow", "--exclude", ".git" },
-              previewer = false,
-            })
-          end,
-        },
-        {
-          "<leader>fq",
-          function()
-            local cwd = vim.fn.expand("%:p:h")
-            if vim.bo.filetype == "oil" then
-              cwd = string.gsub(cwd, "^oil://", "")
-            end
-            builtin.find_files({
-              cwd = cwd,
-              find_command = { "fd", "--type", "d", "--hidden", "--follow", "--exclude", ".git" },
-              previewer = false,
-            })
-          end,
-        },
-        {
-          "<leader>fa",
-          function()
-            local cwd = vim.fn.expand("%:p:h")
-            if vim.bo.filetype == "oil" then
-              cwd = string.gsub(cwd, "^oil://", "")
-            end
-            builtin.find_files({
-              cwd = cwd,
-              previewer = false,
-            })
-          end,
-        },
-        {
-          "<leader>/",
-          function()
-            builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-              winblend = 10,
-              previewer = false,
-            }))
-          end,
-          desc = "Find recently opened files",
-        },
+        }
       }
     end,
     opts = function()
